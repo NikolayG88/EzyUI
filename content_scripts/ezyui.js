0ac39ac100f64a21137ea7537a8c4f241f94a8dc
NikolayG88 <uriev@abv.bg>
2018-03-21 09:02:13 +0200

    Basic context menu

--- content_scripts/ezyui.js
+++ content_scripts/ezyui.js
@@ -43,7 +43,9 @@ var gridWorker = (function(){
 			that = this;
 		}
 		
-		that.each(node);
+		if(that.each != undefined){
+			that.each(node);
+		}
 		
 		if(node == undefined){
 			_maskGrid(document.body, colLvl);
@@ -56,7 +58,9 @@ var gridWorker = (function(){
 		
 		if(_isBSColumn(node)){
 			 _maskBSColumn(node, colLvl);
-			 
+			 if(that.eachColumn != undefined){
+				 that.eachColumn(node);
+			 }
 			 colLvl++;
 		}else{
 			_maskNonBSElement(node);
@@ -78,32 +82,138 @@ var gridWorker = (function(){
 		eachColumn: undefined,
 		maskGrid: _maskGrid
 	};
-	
-
 }());
+
+var bsGridContextMenu = (function(){
+	//TODO: count columns and distribute the sizes evenly
+	
+	var colContextMenu = undefined;
+	
+	var colMenuHtml = `
+		<style>
+			#colContextMenu{
+				position:fixed;
+				visibility:visible;
+				width:200px;
+				height:100px;
+				background-color:#F2F2F2;
+				top:0px;
+				left:0px;
+				z-index:1000;
+			}
+
+			#colContextMenu ul{
+				padding:5px;
+				list-style-type:none;
+			}
+
+			#colContextMenu li{
+				margin:5px;
+				color:black;
+				cursor:arrow;
+				-webkit-user-select: none;  
+				-moz-user-select: none;    
+				-ms-user-select: none;      
+				user-select: none;
+			}
+
+			#colContextMenu li:hover{
+				background-color:#D7D7D7;
+			}
+
+			#colContextMenu hr{
+				border-top:1px solid #D7D7D7;
+				margin:0px;
+				margin-left:5px;
+				margin-right:5px;
+				margin-bottom:5px;
+			}
+			
+		</style>
+		<div>
+			<ul>
+				<li>Insert Rows</li>
+				<hr />
+				<li>Insert Columns</li>
+				<hr />
+			</ul>
+		</div>`;
+
+	function _openColMenu(event){
+		
+		try{
+			if(colContextMenu == undefined){
+				colContextMenu = document.createElement("div");
+
+				colContextMenu.innerHTML = colMenuHtml;
+				colContextMenu.id = "colContextMenu";
+				colContextMenu.style.visibility = "visible";
+
+				document.body.appendChild(colContextMenu);
+
+				window.onclick = function(evt){
+					if(evt.button == 0){
+						colContextMenu.style.visibility = "hidden";
+					}
+				};
+			}else{
+				colContextMenu.style.visibility = "visible"; 
+			}
+			
+			colContextMenu.style.top = event.clientY + 'px';
+			colContextMenu.style.left = event.clientX + 'px';
+
+			
+			event.stopPropagation();
+		}catch(err){
+			console.log(err.message);
+		}
+	};
+	
+	function _onColContextMenuMask(event){
+		event.preventDefault();
+		_openColMenu(event);
+	}
+	
+	return {
+		onColContextMenuMask: _onColContextMenuMask
+	};
+	
+})();
 
 (function() {
 	
 	document.levelColorBase = ['#C2561A', '#DA611E', '#F16C20', '#F58A4B'];
  
-  if (window.hasRun) {
-    return;
-  }
+	if (window.hasRun) {
+		return;
+	}
+	
+  	window.hasRun = true;
   
-  window.hasRun = true;
+  	var gw = gridWorker;
   
-  var gr = gridWorker;
-  
-  gr.each = function(node){
-	  console.log(node);
-  };
-  gr.maskGrid();
-  
-  /**
-   * Listen for messages from the background script.
-  */
-  browser.runtime.onMessage.addListener((message) => {
-   alert(message);
-  });
+	var nodeIdx = 0;
+
+	gw.eachColumn = function(colNode){
+		colNode.oncontextmenu = bsGridContextMenu.onColContextMenuMask;
+		if(colNode.id != "" && colNode.id >= nodeIdx){
+			console.log("Duplicate node " + colNode.id);
+		}
+		else{
+			colNode.id = nodeIdx;
+			nodeIdx++;
+		}
+		
+		//console.log(colNode);
+	};
+	gw.maskGrid();
+	
+	/**
+	 * Listen for messages from the background script.
+	 */
+	browser.runtime.onMessage.addListener((message) => {
+	alert(message);
+	});
 
 })();
